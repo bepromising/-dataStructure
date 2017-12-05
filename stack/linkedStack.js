a20140a976e9e45cb276e1ca7d9230ffbefc4ab4
cxi <bepromising_cxi@163.com>
2017-12-05 20:33:32 +0800

    fix

--- stack/linkedStack.js
+++ stack/linkedStack.js
@@ -5,21 +5,21 @@ class Node {
   }
 }
 
-class LinkedStack { // 链式存储
+class LinkedStack {
   constructor () {
-    this.top = null; // 栈顶指针
-    this.bottom = null; // 栈底指针
+    this.top = null;          // 栈顶指针
+    this.bottom = null;       // 栈底指针
     this.length = 0;
   }
 
   push (element) {
     let node = new Node(element);
 
-    if (!this.top) { // 栈顶为空
+    if (!this.top) {          // 栈顶为空
       this.top = this.bottom = node; // 栈顶为node
     } else {
       let front = this.top;
-      this.top = front.next = node; // 前一个的next 和 栈顶 都指向 node
+      this.top = front.next = node;  // 前一个的next 和 栈顶 都指向 node
     }
     this.length++;
     console.log('压栈成功啦！');
@@ -27,30 +27,31 @@ class LinkedStack { // 链式存储
 
   pop () {
     let node = this.bottom, front;
-    if (!node) { // 栈底为空？ 那就是空栈咯。
+    if (!node) {                     // 栈底为空？ 那就是空栈咯。
       console.log('null stack');
     } else {
-      while (node.next) { // 当node.next为null，退出循环
-        front = node; // 当node.next不为null，那么front指向这个node
-        node = front.next; // node 重新指向 front的下一个
+      while (node.next) {            // 当node.next为null，退出循环
+        front = node;                // 当node.next不为null，那么front指向这个node
+        node = front.next;           // node 重新指向 front的下一个
       }
-      this.top = front; //node的next为null时，说明node为栈顶了，那么栈顶要指向前一个front
-      front.next = null; // front.next就为null了，不能指向node了，因为node出栈咯。
+      this.top = front;              //node的next为null时，说明node为栈顶了，那么栈顶要指向前一个front
+      front.next = null;             // front.next就为null了，不能指向node了，因为node出栈咯。
     }
     this.length--;
     console.log('出栈成功！');
   }
 
-  print (node) {
-    if (!node) {
-      node = this.bottom;
-      console.log(this.bottom);
+  print () {
+    let arr = [this.bottom];
+    let node = this.bottom;
+    while (node.next) {
+      node = node.next;
+      arr.push(node);
     }
-    if (node.next) {
-      console.log(node.next);
-      if (!node.next.next) return node.next.element;
-      this.print(node.next);
-    }
+    arr.map( (x, index) => {
+      console.log(`第${index + 1}个节点是:`);
+      console.log(x);
+    });
   }
 }
 
